3a493b7da428eae2e0560d7a147c9a863e6e553d
anserliu <anserliu@tencent.com>
2017-12-21 20:24:25 +0800

    webpack同步打包原理

--- wepack_runprocess/sync/minifest.js
+++ wepack_runprocess/sync/minifest.js
@@ -1,24 +1,39 @@
 (function (modules) {
     window.webpackJsonp = function (chunkIds, moreModules, executeModules) {
-        // moreModules['a'](module, module.exports, 0)
         // 遍历 moreModules
         var moduleId
-        for(moduleId in moreModules){
-            if(Object.prototype.hasOwnProperty.call(moreModules,moduleId)){
+        for (moduleId in moreModules) {
+            if (Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+                // 模块函数赋值给 modules[moduleId] ??
                 modules[moduleId] = moreModules[moduleId]
             }
         }
         // 执行入口函数
-        if(executeModules){
-            for(var i = 0;i < executeModules.length;i++){
+        if (executeModules) {
+            for (var i = 0; i < executeModules.length; i++) {
                 var result = __webpack__require__(executeModules[i])
             }
         }
         return result
     }
 
+    var installedModules = {}
     // 定义__webpack__require__ 函数
-    __webpack__require__ = function(moduleId){
-        
+    __webpack__require__ = function (moduleId) {
+        if (installedModules[moduleId]) {
+            return installedModules[moduleId].exports
+        }
+        var module = installedModules[moduleId] = {
+            l: false, // 模块是否加载 false 未加载
+            id: moduleId,
+            exports: {}
+        }
+
+        var result
+        // this == module.exports ??
+        // 导出 module 与 module.exports 
+        modules[moduleId].call(module.exports, module, module.exports, __webpack__require__)
+        module.l = true
+        return module.exports
     }
 })([])
